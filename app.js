9909eec4015c692c84500d8bd41f9ed7ea47c60e
Jethet <henriette.hettinga@protonmail.com>
2021-06-07 16:04:18 +0200

    Get user and choice using prompt

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
-const player = ""
+var prompt = require("prompt");
 
-const grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-grid.map((item) => {
-  console.log(item);
-})
+prompt.start();
+
+let player = "";
+let choice = null
+
+const grid = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+function createGrid() {
+  grid.map((item) => {
+    console.log(item);
+  });
+}
+
+function checkSquare() {
+  prompt.get(["username", "choice"], function (err, result) {
+    player = result.username
+    choice = result.choice
+    console.log(player, choice);
+  });
+}
+
+
+checkSquare()
+
+// handleKeyDown = (event) => {
+//   if (event.keyCode === 88) {
+//     console.log("X");
+//   }
+//   if (event.keyCode === 79) {
+//     console.log("O");
+//   }
+// };
+
+// window.addEventListener("keydown", handleKeySpace);
