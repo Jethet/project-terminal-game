10ae808925a1f79eb928920d575fb41ca623a422
Jethet <henriette.hettinga@protonmail.com>
2021-08-29 14:54:22 +0200

    Add first component; reuse HTML and CSS from Node version

--- react-tic-tac-toe/src/GameGrid.jsx
+++ react-tic-tac-toe/src/GameGrid.jsx
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="table-container">
+import React from 'react';
+
+const GameGrid = () => {
+  return ( 
+    
+    <div class="table-container">
     <table class="table-grid">  
       <tr class="first-row">  
         <td class="cell"></td>  
@@ -26,7 +21,8 @@
         <td class="cell"></td>  
       </tr>  
     </table>
-  </div>
-  
-</body>
-</html>
+    </div>
+   );
+}
+ 
+export default GameGrid;
