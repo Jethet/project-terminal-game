4e7265e4540284bc7f8d7ad215850c157c2e82c4
Jethet <henriette.hettinga@protonmail.com>
2021-09-03 07:31:34 +0200

    Clean up code

--- node-terminal-version/app.js
+++ node-terminal-version/app.js
@@ -4,7 +4,13 @@ prompt.start();
 
 let player = "";
 let choice = null;
-let index = null
+let index = null;
+
+function printInstructions() {
+  console.log(
+    "Enter your name as (capital) X or (capital) O.\nYour choice of square can be a number between 1 and 9.\nYou always have to enter a name and choice.\nThe game will end if you do not follow these instructions.\n"
+  );
+}
 
 const grid = [
   [1, 2, 3],
@@ -16,8 +22,8 @@ function createGrid() {
   grid.map((item) => {
     console.log(item);
   });
+  getChoice();
 }
-
 
 function setSquare() {
   if (choice === "1" || choice === "2" || choice === "3") {
@@ -30,27 +36,49 @@ function setSquare() {
     choice = choice - 7;
     index = grid[2];
   }
+
   if (player === "X") {
     index[choice] = "X";
   } else if (player === "O") {
     index[choice] = "O";
   }
   createGrid();
-  getChoice();
+  // getChoice();
 }
-
 
 function getChoice() {
   prompt.get(["name", "choice"], function (err, result) {
     player = result.name;
     choice = result.choice;
-    setSquare();
+    validateChoice();
   });
 }
 
+function validateChoice() {
+  if (!player || !choice) {
+    errorAlert();
+  }
+  // if (player !== "O" || player !== "X") {
+  //   errorAlert();
+  // }
+  if (choice < 1 || choice > 9) {
+    errorAlert();
+  }
+  setSquare();
+}
+
+function errorAlert() {
+  choice = null;
+  console.log(
+    "There is an error. Make sure you enter your player sign: capital X or capital O. The choice of square has to be between 1 and 9. Both name and choice must be entered."
+  );
+  return;
+}
+
 function playGame() {
+  printInstructions();
   createGrid();
-  getChoice();
+  // getChoice();
 }
 
 playGame();
